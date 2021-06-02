6425f20d3942c044ed922e4673bbacc9f0d514d7
Shreya <62697529+Shreya869@users.noreply.github.com>
2021-06-03 00:53:47 +0530

    Create watermelon.cpp

---
+++ codeforces/watermelon.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ int main()
+ {
+     int weight;
+
+cin>>weight;
+
+if(weight%2==0 & weight>2)  //odd
+{
+    cout<<"YES"<<endl;
+
+}
+
+else //even
+{
+    cout<<"NO"<<endl;
+
+}
+
+return 0;
+ }
